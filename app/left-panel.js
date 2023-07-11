fd3da3e0ff7a01e2a56f31288368666b3fe3255d
bmo <belindacfmo@gmail.com>
2023-07-10 17:46:02 -0700

    Update left panel with accurate information

--- app/left-panel.js
+++ app/left-panel.js
@@ -13,18 +13,9 @@ import {
   import { classNames } from './utils'
 
 const navigation = [
-    { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-    { name: 'Team', href: '#', icon: UsersIcon, current: false },
-    { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-    { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-    { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
-    { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
-]
-
-const teams = [
-    { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
-    { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
-    { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
+    { name: 'Agents', href: '#', icon: UsersIcon, current: true },
+    { name: 'Runs', href: '#', icon: FolderIcon, current: false },
+    { name: 'Evals', href: '#', icon: ChartPieIcon, current: false },
 ]
 
 export default function LeftPanel() {
@@ -104,29 +95,6 @@ export default function LeftPanel() {
                                 ))}
                                 </ul>
                             </li>
-                            <li>
-                                <div className="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
-                                <ul role="list" className="-mx-2 mt-2 space-y-1">
-                                {teams.map((team) => (
-                                    <li key={team.name}>
-                                    <a
-                                        href={team.href}
-                                        className={classNames(
-                                        team.current
-                                            ? 'bg-gray-800 text-white'
-                                            : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                                        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                                        )}
-                                    >
-                                        <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                                        {team.initial}
-                                        </span>
-                                        <span className="truncate">{team.name}</span>
-                                    </a>
-                                    </li>
-                                ))}
-                                </ul>
-                            </li>
                             </ul>
                         </nav>
                         </div>
@@ -169,29 +137,6 @@ export default function LeftPanel() {
                     ))}
                     </ul>
                 </li>
-                <li>
-                    <div className="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
-                    <ul role="list" className="-mx-2 mt-2 space-y-1">
-                    {teams.map((team) => (
-                        <li key={team.name}>
-                        <a
-                            href={team.href}
-                            className={classNames(
-                            team.current
-                                ? 'bg-gray-800 text-white'
-                                : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                            'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                            )}
-                        >
-                            <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                            {team.initial}
-                            </span>
-                            <span className="truncate">{team.name}</span>
-                        </a>
-                        </li>
-                    ))}
-                    </ul>
-                </li>
                 <li className="-mx-6 mt-auto">
                     <a
                     href="#"
@@ -203,7 +148,7 @@ export default function LeftPanel() {
                         alt=""
                     />
                     <span className="sr-only">Your profile</span>
-                    <span aria-hidden="true">Tom Cook</span>
+                    <span aria-hidden="true">Jordan Wick</span>
                     </a>
                 </li>
                 </ul>
